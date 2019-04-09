3b9ea8eeee5e255fd54eec982d9621777f4270a5
Brendan Thomas <bt331609@gmail.com>
2019-04-08 21:30:05 -0400

    create users and use a repository as a data source

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,23 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository
+  """
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Jon", username: "jonsnow", password: "north"},
+     %Rumbl.User{id: "2", name: "Jamie", username: "jamielannister", password: "cersei"},
+     %Rumbl.User{id: "3", name: "Arya", username: "aryastark", password: "assassain"}]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
